610ffa2ca073fc544d87cba80b7fd1974126c889
jessicakwalters <jessicakwalters@gmail.com>
2019-10-15 13:44:38 -0700

    update to pass token via cookie, not header

--- src/auth/router.js
+++ src/auth/router.js
@@ -11,7 +11,8 @@ const authRouter = express.Router();
 authRouter.get('/oauth', (req,res,next) => {
   oauth.authorize(req)
     .then( token => {
-      res.redirect(301, `http://localhost:3000/?token=${token}`);
+      res.cookie('X-401d19-OAuth-token',token);
+      res.redirect(301, process.env.CLIENT_URL);
     })
     .catch(next);
 });
