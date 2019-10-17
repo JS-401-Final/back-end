601c2ebeb4588f5dd51e8323661c7131ac2adbb0
Benjamin Clark <benisin@gmail.com>
2019-10-17 11:33:43 -0700

    documented all routes

--- src/routes/users-router.js
+++ src/routes/users-router.js
@@ -1,21 +1,45 @@
 'use strict';
+/**
+ * @module router/users
+ * @requires express
+ */
 
 const express = require('express');
 const router = express.Router();
 const { prisma } = require('../../prisma-database/generated/prisma-client');
 
-//// ====== USER Routes ======
 
+/**
+ * This function creates a new user in the database
+ * @function POST
+ * @param {string} path - Express Path
+ * @param {function} callback - express callback
+ * @returns { (Object | Error) } - the newly created user object
+ */
 router.post('/user', async (req, res) => {
   const newUser = await prisma.createUser(req.body);
   res.json(newUser);
 });
 
+/**
+ * This function gets all user data from database
+ * @function GET
+ * @param {string} path - express path
+ * @param {function} callback - express callback
+ * @returns { (Array | Error) } - an array of all users
+ */
 router.get('/users', async (req, res) => {
   const users = await prisma.users();
   res.json(users);
 });
 
+/**
+ * This function gets a user from database
+ * @function GET/:id
+ * @param {string} path - express path
+ * @param {function} callback - express callback
+ * @returns { (Object | Error) } - a single user object
+ */
 router.get('/user/:id', async (req, res) => {
   const user = await prisma.user({ id: req.params.id });
   res.json(user);
